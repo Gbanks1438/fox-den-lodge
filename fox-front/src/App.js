e235acf52715bcd27c2086cf1a605987446dca61
Gregory Banks <gbanks1438@gmail.com>
2023-01-02 20:39:01 -0500

    scaffolding building

--- fox-front/src/App.js
+++ fox-front/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+import image from './images/Big-Bowman-Pond.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+       <div>
+          <h1>Frontend stuff</h1>
+        </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Frontend stuff
-        </p>
+        <img src={image} className="lodge-image" alt="lodge on Big Bowman Pond" />
       </header>
     </div>
   );
